149b5b5453769ffd4cbc1ff1ad63037d8b2eed1a
Kenay <silvakenay5@gmail.com>
2026-05-04 14:30:10 -0700

    refactor: update automated printing batch script paths and execution parameters

--- ImpresionAutomatica.bat
+++ ImpresionAutomatica.bat
@@ -13,13 +13,13 @@
 
 :: Intentar ruta estándar de Chrome
 if exist "C:\Program Files\Google\Chrome\Application\chrome.exe" (
-    start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --kiosk-printing --autoplay-policy=no-user-gesture-required --user-data-dir="%temp%\ChromePrintProfile" "https://laesquinadelsushi.vercel.app/admin.html"
+    start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --kiosk-printing --autoplay-policy=no-user-gesture-required --user-data-dir="%temp%\ChromePrintProfile" "https://laesquinadelsushi.com/admin.html"
     exit
 )
 
 :: Intentar ruta x86
 if exist "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" (
-    start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --kiosk-printing --autoplay-policy=no-user-gesture-required --user-data-dir="%temp%\ChromePrintProfile" "https://laesquinadelsushi.vercel.app/admin.html"
+    start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --kiosk-printing --autoplay-policy=no-user-gesture-required --user-data-dir="%temp%\ChromePrintProfile" "https://laesquinadelsushi.com/admin.html"
     exit
 )
 
